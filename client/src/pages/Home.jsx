90d84f376873cf51b02a4884367a712c23e2b316
Adithya-U <adithya2k3@gmail.com>
2025-07-24 12:48:27 +0530

    made images swipable on mobile

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,92 +1,88 @@
 import React, { useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
-import im1 from '../assets/jv.png'
-import im2 from '../assets/ren.png'
-import im3 from '../assets/de.png'
+import { useSwipeable } from 'react-swipeable';
+import im1 from '../assets/jv.png';
+import im2 from '../assets/ren.png';
+import im3 from '../assets/de.png';
 import About from './About';
 import Expertise from './expertise';
+
 export default function Home() {
-  const slides = [
-    {
-      url: im3,
-    },
-    {
-      url: im2,
-    },
-    {
-      url: im1,
-    },
-
-  ];
-
+  const slides = [{ url: im3 }, { url: im2 }, { url: im1 }];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    const newIndex = currentIndex === 0 ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    const newIndex = currentIndex === slides.length - 1 ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
 
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
-  const handleSwipe = (e) => {
-    if (e.direction === 'Left') {
-      nextSlide();
-    } else if (e.direction === 'Right') {
-      prevSlide();
-    }
-  };    
+  const goToSlide = (index) => setCurrentIndex(index);
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: nextSlide,
+    onSwipedRight: prevSlide,
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  });
+
   return (
-    <div className = 'h-full min-h-screen bg-[#fffff6]'>
+    <div className='h-full min-h-screen bg-[#fffff6]'>
+      <div
+        {...swipeHandlers}
+        className='h-[780px] w-full m-auto py-16 px-2 relative group'
+      >
+        <div
+          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+          className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+        ></div>
 
-       <div className=' h-[780px] w-full m-auto py-16 px-2 relative group'>
-       <div
-        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      ></div>
-            {/* Left Arrow */}
-            <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        {/* Left Arrow */}
+        <div className='absolute top-[50%] -translate-y-1/2 left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer md:group-hover:block md:hidden block'>
+          <BsChevronCompactLeft onClick={prevSlide} size={30} />
+        </div>
+
+        {/* Right Arrow */}
+        <div className='absolute top-[50%] -translate-y-1/2 right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer md:group-hover:block md:hidden block'>
+          <BsChevronCompactRight onClick={nextSlide} size={30} />
+        </div>
+
+        <div className='flex top-4 justify-center py-2'>
+          {slides.map((_, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className={`text-2xl cursor-pointer ${
+                currentIndex === slideIndex ? 'text-black' : 'text-gray-400'
+              }`}
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
       </div>
-      {/* Right Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div>
-      <div className='flex top-4 justify-center py-2'>
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
+
+      <a href="https://wa.me/9380007995?text=I'm%20looking%20for%20a%20flat%20for%20sale">
+        <div className="fixed bg-everglade bottom-0 rounded-full text-tuftbush text-base left-0 p-4 m-10 flex flex-row w-auto align-middle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-10 w-10 px-2"
+            fill="currentColor"
+            viewBox="0 0 24 24"
           >
-            <RxDotFilled />
-          </div>
-        ))}
-      </div>
-      </div>
+            <path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946..." />
+          </svg>
+          <p className="flex-none pt-[0.4rem]">Chat on Whatsapp</p>
+        </div>
+      </a>
 
-      <a href="https://wa.me/9380007995?text=I'm%20looking%20for%20a%20flat%20for%20sale"><div class="fixed bg-everglade bottom-0 rounded-full text-tuftbush text-base left-0 p-4 m-10 flex flex-row w-auto align-middle">
-      <svg 
-    xmlns="http://www.w3.org/2000/svg"
-    class="h-10 w-10 px-2"
-    fill="currentColor"
-    viewBox="0 0 24 24">
-    <path
-      d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />
-  </svg><p className="flex-none pt-[0.4rem]">Chat on Whatsapp</p>
-      </div> </a>
-    <Expertise />
-    <About/>
+      <Expertise />
+      <About />
     </div>
-
-
-  )
+  );
 }
